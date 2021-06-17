351440a6b8b2a58d1d33c94e7ecfb313194f0e9d
Simone Campisi <37673011+simocampi@users.noreply.github.com>
2021-06-17 16:25:28 +0200

    dd

--- SIR_model.py
+++ SIR_model.py
@@ -17,9 +17,9 @@ class Labels:
 
 class SIR:
 
-    def __init__(self, graph, p, T_i, q, i_0):
+    def __init__(self, graph, p, Ti, q, i_0):
        self.p = p # the disease transmission probability
-       self.T_i = T_i # the minimum amount of time a node remains infected
+       self.Ti = Ti # the minimum amount of time a node remains infected
        self.q = q # the recovery probability
 
        if i_0 > nx.number_of_nodes(graph) or i_0 < 0:
@@ -31,13 +31,24 @@ class SIR:
        self.graph_labelled # graph used to process the model (a copy of graph)
 
        self.state_label = "state"
+       self.Ti_label = "Ti"
     
-    # label all node of the graph as Susceptible
+    
     def init_graph(self):
+        # label all node of the graph as Susceptible
         self.graph_labelled = nx.set_node_attributes(self.graph, Labels.S, self.state_label).copy()
 
+        # set the Ti for each node to 0
+        nx.set_node_attributes(self.graph_labelled, 0, self.Ti_label)
+
     def infect_initial_nodes(self):
-        return 
+        
+        # randomly selected nodes
+        infected_nodes = random.sample(self.graph_labelled, self.i_0)
+        
+        # attributes state and Ti of all nodes
+        map_attr = { node : { self.state_label : Labels.I , self.Ti_label : self.Ti } for node in infected_nodes}        
+        nx.set_node_attributes(self.graph_labelled, map_attr)
 
     # plot the graph with the associated colors to the nodes...da decidere come plottare quello big
     def plot_graph(self):  
@@ -48,20 +59,25 @@ class SIR:
         plt.show()
 
     # algorithm stops when all nodes are in R state
+    # return true if all nodes are in state R
     def convergence_test(self):
         
-        # if node[self.state_label] == Labels.S or node[self.state_label] == Labels.I
+        # get all nodes
         node_list = [ node for node in self.graph_labelled]
 
-        filtered = list(filter(lambda, node_list))
+        # filter all nodes in state R
+        filtered = list(filter(lambda node: node[self.state_label] == Labels.R, node_list))
         return len(filtered) == 0
     
     def run(self):
         
+        self.infect_initial_nodes()
+        
         while True: ## Repeat the recovery / contagion until all nodes are in compartment R (e.g., Recovered / Removed)
             for infected_node in self.i_list:
                 #from I to R
-
+                if True:
+                    pass
                 #from S to I
                 pass
         pass
